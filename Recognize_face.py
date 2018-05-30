8ad260cbc1142b992854961d2ace0d20a6597927
Gaurav Bodara <gauravbodra20@gmail.com>
2018-05-30 11:06:15 +0530

    Add files via upload

---
+++ Recognize_face.py
@@ -0,0 +1,49 @@
+import numpy as np
+import cv2
+
+
+face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+
+
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+
+#recognizer = cv2.face.EigenFaceRecognizer_create()
+
+#recognizer = cv2.face.FisherFaceRecognizer_create()
+
+recognizer.read("Trainer.yml")
+cap = cv2.VideoCapture(0)
+Id = 0
+font = cv2.FONT_HERSHEY_COMPLEX
+
+while True:
+    ret, img = cap.read()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.2, 5)
+    for (x, y, w, h) in faces:
+
+    
+        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 225, 225), 2)
+
+        
+        Id, conf = recognizer.predict(gray[y:y + h, x:x + w])
+        
+        if Id == 1:
+            Id = "Gaurav"
+        elif Id == 2:
+            Id = "Apeksha"
+        elif Id== 3:
+            Id = "Parth"
+        
+       
+            
+    cv2.rectangle(img, (x - 22, y - 45), (x + w + 15, y - 22), (0, 0, 0), -1)
+    cv2.putText(img,"Name:" + str(Id), (x, y - 22), font, 1, (0, 0, 255), 2)
+    
+    cv2.imshow('img', img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+
+cap.release()
+cv2.destroyAllWindows()
